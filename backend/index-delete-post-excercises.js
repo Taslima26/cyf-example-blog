3d7c2d925f05acc59fd9efb562854e63672fc454
Evan Rolfe <evanrolfe@gmail.com>
2020-08-15 13:55:59 +0100

    Remove .json from paths

--- backend/index-delete-post-excercises.js
+++ backend/index-delete-post-excercises.js
@@ -13,11 +13,11 @@ server.use((req, res, next) => {
 server.use(express.json());
 server.use(cors())
 
-server.get('/posts.json',  (req, res) => {
+server.get('/posts',  (req, res) => {
   res.json(database.posts)
 });
 
-server.get('/posts/:postId.json',  (req, res) => {
+server.get('/posts/:postId',  (req, res) => {
   const { postId } = req.params;
   const post = database.posts.find(p => p.id === parseInt(postId));
   res.json(post)
@@ -29,7 +29,7 @@ server.get('/posts/:postId.json',  (req, res) => {
 // Relation: The user has posts, and not another way around, and just one can be many (plural)
 // so the endpoint will be: /users/{user_id}/posts
 
-server.get('/users/:userId/posts.json',  (req, res) => {
+server.get('/users/:userId/posts',  (req, res) => {
   const { userId } = req.params;
   const posts = database.posts.filter(p => p.user_id === parseInt(userId));
   res.json(posts)
@@ -41,7 +41,7 @@ server.get('/users/:userId/posts.json',  (req, res) => {
 //Relation: The user has posts, and we need one of them (singular)
 // so the endpoint will be: /users/{user_id}/posts/{post_id}
 
-server.get('/users/:userId/posts/:postId.json',  (req, res) => {
+server.get('/users/:userId/posts/:postId',  (req, res) => {
   const { userId, postId } = req.params;
   const posts = database.posts.filter(p => p.user_id === parseInt(userId));
   const post = posts.find(p => p.id === parseInt(postId));
